b1d65a61795da754e8e200109d4fbb12c238cfd1
kimgy44 <kimgy44@naver.com>
2022-06-22 15:05:37 +0900

    로그인표,문제풀이

--- day1/hello.js
+++ day1/hello.js
@@ -3,6 +3,6 @@ let colors = require('colors');
 console.log(colors.rainbow('Hello NodeJS!!!'));
 console.log(colors.green('Hello NodeJS!!!'));
 console.log(colors.red('Hello NodeJS!!!'));
-console.log(colors.red('Hello NodeJS!!!'));  
+console.log(colors.yellow('Hello NodeJS!!!'));
 let a = 5;
 console.log(a);  
